d20919ab739ae6c7da1e229d40dc8b5596d1ed22
James Royer <walkingalchemy@gmail.com>
2017-10-24 18:42:37 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,8 +1,16 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(dwarves)# code an argument here
+  dwarves.map do |dwarf|
+    puts "#{dwarves.find_index(dwarf) + 1}. #{dwarf}"
+  end
+    .join(" ")
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(elements)
+  elements.map do |element|
+    element = element.split("").push("!")
+    element[0].upcase
+    element.join
+  end
   # Your code here
 end
 
