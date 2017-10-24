ca6b0f833afe61951e13283a3e8312e0a2f099d5
James Royer <walkingalchemy@gmail.com>
2017-10-24 18:49:51 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,8 +14,8 @@ def summon_captain_planet(elements)
   # Your code here
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.any? { |call| call > 4 }
 end
 
 def find_the_cheese# code an argument here
